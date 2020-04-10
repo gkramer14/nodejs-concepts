584fdcdbe0683a50f740568d015b9d66f4d65f36
gkramer14 <cetecdesp@gmail.com>
2020-04-10 15:06:10 -0300

    Programmed the principal functions of our API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,101 @@ app.use(cors());
 
 const repositories = [];
 
+function checkId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Unrecognized ID' });
+  }
+  
+  return next();
+}
+
+app.use('/repositories/:id', checkId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  if (!title || !url || !techs) {
+    return response.status(400).json({ error: "Check all the informations at the request's body" });
+  }
+
+  const repo = {
+    id: uuid(),
+    title, 
+    url, 
+    techs, 
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  return response.status(201).json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  if (!title || !url || !techs) {
+    return response.status(400).json({ error: "Check all the informations at the request's body" });
+  }
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  const repo = {
+    ...repositories[repoIndex],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repoIndex] = repo;
+
+  return response.status(200).json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  const likes = repositories[repoIndex].likes + 1;
+
+  const repo = {
+    ...repositories[repoIndex],
+    likes,
+  };
+
+  repositories[repoIndex] = repo;
+
+  return response.status(200).json(repo);
 });
 
 module.exports = app;
